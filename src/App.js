e0c9c5ff74e9f2d984110a7069bc550f806aa3a8
mika-alpha <yuukeikioku@gmail.com>
2022-10-26 16:21:13 -0500

    Add basic info states and their handlers to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,47 @@ import React, { Component } from "react";
 
 class App extends Component{
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      basic: {
+        name: '',
+        email: '',
+        phone: '',
+      },
+    }
+  }
+
+  handleNameChange(name) {
+    this.setState({
+      basic: {
+        name: name,
+        email: this.state.basic.email,
+        phone: this.state.basic.phone
+      }
+    })
+  }
+
+  handleEmailChange(email) {
+    this.setState({
+      basic: {
+        name: this.state.basic.name,
+        email: email,
+        phone: this.state.basic.phone
+      }
+    })
+  }
+
+  handlePhoneChange(phone) {
+    this.setState({
+      basic: {
+        name: this.state.basic.name,
+        email: this.state.basic.email,
+        phone: phone
+      }
+    })
+  }
+
 
   render() {
     return (
