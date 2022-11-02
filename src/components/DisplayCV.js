e0102e7b06541ce213daa1c5ca09196111683e33
mika-alpha <yuukeikioku@gmail.com>
2022-11-01 20:09:09 -0500

    Refactor DisplayCV into functional component

--- src/components/DisplayCV.js
+++ src/components/DisplayCV.js
@@ -1,80 +1,73 @@
-import React, { Component } from "react";
+import React from "react";
 import uniqid from 'uniqid';
 
-class DisplayCV extends Component{
+const DisplayCV = (props) => {
 
-    formatDate(date) {
+    const formatDate = (date) =>  {
         const alpha = date.split('-');
         const months = ["January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"];
         return `${months[+alpha[1]-1]}, ${alpha[0]}`
     }
 
-    handleEdit() {
-        document.getElementById('form').classList.toggle('')
+    let className = 'display ';
+    if (!props.display) {
+        className += 'hidden';
     }
 
-    render() {
-
-        let className = 'display ';
-        if (!this.props.display) {
-            className += 'hidden';
-        }
-
-        const education = [];
-        this.props.education.forEach((school) => {
-            education.push(
-                <div key={uniqid()} className='info'>
-                    <div className="header">
-                        <h4>{school.name}</h4>
-                        <span> {this.formatDate(school.start)} - {this.formatDate(school.end)}</span>
-                    </div>
-                    <h5>{school.title}</h5>
+    const education = [];
+    props.education.forEach((school) => {
+        education.push(
+            <div key={uniqid()} className='info'>
+                <div className="header">
+                    <h4>{school.name}</h4>
+                    <span> {formatDate(school.start)} - {formatDate(school.end)}</span>
                 </div>
-            )
-        })
-
-        const experience = [];
-        this.props.experience.forEach((job) => {
-            experience.push(
-                <div key={uniqid()} className='info'>
-                    <div className="header">
-                        <h4>{job.position}</h4>
-                        <span>{this.formatDate(job.start)} - {this.formatDate(job.end)}</span>
-                    </div>
-                    <h5>{job.company}</h5>
-                    <p>{job.tasks}</p>
-                </div>
-            )
-        })
-
-        return (
-            <div className={className}>
-                <div id="cv-basic">
-                    <h1>{this.props.name}</h1>
-                    <div>
-                        <h2>{this.props.phone}</h2>
-                        <h2>{this.props.email}</h2>
-                    </div>
-                </div>
-
-                <div>
-                    <h3>Education</h3>
-                    <hr></hr>
-                    {education}
-                </div>
-
-                <div>
-                    <h3>Experience</h3>
-                    <hr></hr>
-                    {experience}
-                </div>
-                <button type="button"
-                    onClick={this.props.alternate}
-                className='display-button'>Edit</button>
+                <h5>{school.title}</h5>
             </div>
         )
-    }
+    })
+
+    const experience = [];
+    props.experience.forEach((job) => {
+        experience.push(
+            <div key={uniqid()} className='info'>
+                <div className="header">
+                    <h4>{job.position}</h4>
+                    <span>{formatDate(job.start)} - {formatDate(job.end)}</span>
+                </div>
+                <h5>{job.company}</h5>
+                <p>{job.tasks}</p>
+            </div>
+        )
+    })
+
+    return (
+        <div className={className}>
+            <div id="cv-basic">
+                <h1>{props.name}</h1>
+                <div>
+                    <h2>{props.phone}</h2>
+                    <h2>{props.email}</h2>
+                </div>
+            </div>
+
+            <div>
+                <h3>Education</h3>
+                <hr></hr>
+                {education}
+            </div>
+
+            <div>
+                <h3>Experience</h3>
+                <hr></hr>
+                {experience}
+            </div>
+            <button type="button"
+                onClick={props.alternate}
+                className='display-button'>Edit</button>
+        </div>
+    )
 }
 
 export default DisplayCV;
