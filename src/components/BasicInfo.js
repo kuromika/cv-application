5dfc944c33d5aef8c57efc52166b30dbd69d6ea9
mika-alpha <yuukeikioku@gmail.com>
2022-11-01 19:59:06 -0500

    Refactor BasicInfo into Functional Component

--- src/components/BasicInfo.js
+++ src/components/BasicInfo.js
@@ -1,37 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import InfoInput from "./InfoInput";
 
-class BasicInfo extends Component {
+const BasicInfo = (props) =>  {
 
-   
-    render() {
-        return (
-            <div id='basic-info'>
-                <h2>Basic Information</h2>
-                <div className="basic-input">
-                    <InfoInput
-                        cb={this.props.cb}
-                        type="text"
-                        name='name'
-                        label='Full name: '
+    return (
+        <div id='basic-info'>
+            <h2>Basic Information</h2>
+            <div className="basic-input">
+                <InfoInput
+                    cb={props.cb}
+                    type="text"
+                    name='name'
+                    label='Full name: '
+                />
+                <InfoInput
+                    cb={props.cb}
+                    type="email"
+                    name='email'
+                    label='Email: '
+                />
+                <InfoInput
+                    cb={props.cb}
+                    type="tel"
+                    name='phone'
+                    label='Phone: '
+                    pattern = "+\d"
                     />
-                    <InfoInput
-                        cb={this.props.cb}
-                        type="email"
-                        name='email'
-                        label='Email: '
-                    />
-                    <InfoInput
-                        cb={this.props.cb}
-                        type="tel"
-                        name='phone'
-                        label='Phone: '
-                        pattern = "+\d"
-                        />
-                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default BasicInfo;
